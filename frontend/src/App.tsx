d1c7920e2ba01db197bbab9eb8428acff21faf80
3p1demicz <3p1demicz@gmail.com>
2023-04-19 14:25:12 +0200

    started

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import IndividualMovie from './movies/IndividualMovie';
+import MoviesList from './movies/MoviesList';
+import { landingPageDTO, movieDTO } from './movies/movies.model';
 
 function App() {
+  const [movies, setMovies] = useState<landingPageDTO>({});
+
+  useEffect(()=> {
+    const timerId = setTimeout(() => {
+      setMovies({
+        inTheaters:[{
+          id: 1,
+          title: 'Movie name',
+          poster: 'https://fair-delivery.com/assets/woo.png'
+        },
+        {
+          id: 2,
+          title: 'Movie name 2',
+          poster: 'https://fair-delivery.com/assets/woo.png'
+        }
+      ],
+      upcomingReleases:  [{
+        id: 3,
+        title: 'Movie name',
+        poster: 'https://fair-delivery.com/assets/api.png'
+      },
+      {
+        id: 4,
+        title: 'Movie name 2',
+        poster: 'https://fair-delivery.com/assets/api.png'
+      }
+      ]
+      })
+
+    }, 1000);
+    return () => clearTimeout(timerId);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hello world</h1>
+  
+    <h3>In theaters</h3>
+
+    <MoviesList movies={movies.inTheaters} />
+
+    <h3>Upcoming</h3>
+    <MoviesList movies={movies.upcomingReleases} />
+    </>
   );
 }
 
---
+++ frontend/src/utils/Loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading(){
+    return <img width="100px" height="100px" src="https://www.wpfaster.org/wp-content/uploads/2013/06/loading-gif.gif"></img>
+}
