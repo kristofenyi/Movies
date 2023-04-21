85791381592b0c3e5daf696a963b8e125aeb509f
3p1demicz <3p1demicz@gmail.com>
2023-04-21 12:50:45 +0200

    progres

--- frontend/src/genres/CreateGenre.tsx
+++ frontend/src/genres/CreateGenre.tsx
@@ -1,19 +1,35 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import Button from "../utils/Button";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import * as Yup from 'yup';
+import TextField from "../forms/TextField";
 
 export default function CreateGenre() {
   const navigate = useNavigate();
   return (
     <>
       <h3>Create Genre</h3>
-      <Button
-        onClick={() => {
-          // .. saving to DB
-          navigate("/genres");
-        }}
+
+      <Formik initialValues={{
+        name:''
+      }}
+      onSubmit={value => {
+        //when form is posted
+        console.log(value);
+      }}
+      validationSchema={Yup.object({
+        name: Yup.string().required('This field is required')
+      })}
       >
-        Save changes
-      </Button>
+        <Form>
+          <TextField displayName="Name" field="name"/>
+              <br/>
+              <Button type='submit'>Save changes</Button>
+              <Link className="btn btn-secondary" to="/genres">Cancel</Link>
+        </Form>
+      </Formik>
+
     </>
   );
 }
--- frontend/src/utils/Button.tsx
+++ frontend/src/utils/Button.tsx
@@ -1,10 +1,15 @@
 export default function Button(props: buttonProps)
 {
     return <button onClick={props.onClick} 
-    className="btn btn-primary">{props.children}
+    className="btn btn-primary" type={props.type}>{props.children}
     </button>
 }
 interface buttonProps{
     children:React.ReactNode;
-    onClick(): void;
+    onClick?(): void;
+    type: "button" | "submit";
+}
+
+Button.defaultProps = {
+    type:"button"
 }
